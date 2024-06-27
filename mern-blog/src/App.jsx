c840d58907141b29bee9d4fbfa9112e358cd1ad2
armanahad6207 <armanahad7549@gmail.com>
2024-06-27 18:18:33 +0530

    create pages and install router-dom

--- mern-blog/src/App.jsx
+++ mern-blog/src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Dashboard from "./Pages/Dashboard";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+
 export default function App() {
   return (
-    <div>
-      <h1 className="text-red-500 text-3xl"> app</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <></>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
