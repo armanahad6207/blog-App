722ff92d1d24469d98f22b5f1b123abb0b2aa329
armanahad6207 <armanahad7549@gmail.com>
2024-06-29 17:22:46 +0530

    create routes ,model and controller

--- api/index.js
+++ api/index.js
@@ -1,5 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import userRoute from "./routes/user.route.js";
+
+//connect to database
+mongoose
+  .connect("mongodb://127.0.0.1:27017/Mern-Blog")
+  .then(() => {
+    console.log("connection successfull");
+  })
+  .catch((err) => {
+    console.log("somthing wrong while connectin", err);
+  });
+
 const app = express();
+
+app.use("/api/user", userRoute);
 
 app.listen(3000, () => {
   console.log("server is running in Port 3000");
